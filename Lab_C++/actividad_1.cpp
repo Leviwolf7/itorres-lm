23389709f8399c8cad29d4f1bb6bb384eef698b9
Isai Torres <142456446+Leviwolf7@users.noreply.github.com>
2024-03-05 18:51:30 -0500

    "actividad 1 Java, C# y  C++"

---
+++ Lab_C++/actividad_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int myVariable = 10;
+    const int myConstant = 20;
+
+    cout << "valor de mi variable:" <<myVariable <<endl;
+    cout << "valor de mi constante:" <<myConstant <<endl;
+
+    return 0;
+}
---
+++ Lab_Java/HelloWorld/src/actividad_1.java
@@ -0,0 +1,9 @@
+public class actividad_1 {
+    public static void main(String[] args) {
+        int myVariable = 10;
+        final int myConstant = 20;
+
+        System.out.println("Valor de mi variable: " + myVariable);
+        System.out.println("valor de mi constante: " + myConstant);
+    }
+}
