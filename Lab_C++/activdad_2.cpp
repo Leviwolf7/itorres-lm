80f2357f50ad2f03e0576db353daad113146a115
Isai Torres <142456446+Leviwolf7@users.noreply.github.com>
2024-03-12 21:43:36 -0400

    "added actividad 2"

---
+++ Lab_C++/activdad_2.cpp
@@ -0,0 +1,19 @@
+// Actividad 2
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2, media;
+
+    cout <<"Introduzca el primer numero: ";
+    cin >> num1;
+    cout <<"Introduzca el segundo numero: ";
+    cin >> num2;
+
+    media = (num1 + num2) / 2;
+
+    cout <<"La medida es: " << media <<endl;
+
+    return 0;
+}
